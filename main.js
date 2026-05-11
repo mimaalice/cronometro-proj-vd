dc82c6dd74e359f06c7eb2e206f511a4bb8f79bb
mimaalice <ribeiro.alves.maria@escola.pr.gov.br>
2026-05-11 14:48:09 +0000

    aula 3 correção

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
+    const indice = Number(this.dataset.aba);
+
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
+      abas[j].classList.remove("ativo");
     }
 
-    botoes[i].classList.add("ativo");
+    this.classList.add("ativo");
+    abas[indice].classList.add("ativo");
   };
 }
